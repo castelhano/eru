139359892f45378e089aaa0ac0eba6a6d94d3709
Rafael Alves <castelhano.rafael@gmail.com>
2024-01-31 16:14:28 -0400

    Ajustado elConn type linear

--- eru/static/js/elConnector.js
+++ eru/static/js/elConnector.js
@@ -24,7 +24,7 @@ class jsELConnector{
         let y1 = this.from.offsetTop;
         let fromW = this.from.offsetWidth;
         let fromH = this.from.offsetHeight;
-        let fromE = this.from.offsetLeft + this.from.offsetWidth;
+        let fromE = x1 + fromW; // extremidade a direita do elemento
 
         let x2 = this.to.offsetLeft;
         let y2 = this.to.offsetTop;
@@ -32,18 +32,32 @@ class jsELConnector{
         let toH = this.to.offsetHeight;
         let toE = this.to.offsetLeft + this.to.offsetWidth;
 
-        let deltaX = (x2 - this.toGap) - (fromE + this.fromGap);
+        let deltaX = (x2 - this.toGap) - (fromE + this.fromGap); // Width da linha conectora
+
+        // Adicona icone de inicio e fim do conector
+        if(this.fromIcon && this.fromIcon != ''){
+            this.startIcon = document.createElement('span');this.startIcon.style.position = 'absolute';this.startIcon.innerHTML = this.fromIcon;this.startIcon.style.zIndex = '100';
+            this.startIcon.style.top = `${y1 + (fromH / 2) - (parseInt(this.lineH) / 2) - 14}px`;
+            this.startIcon.style.left = `${fromE + this.fromGap}px`;
+            this.container.appendChild(this.startIcon);
+            this.els.push(this.startIcon);
+        }
+        if(this.toIcon &&  this.toIcon != ''){
+            this.endIcon = document.createElement('span');this.endIcon.style.position = 'absolute';this.endIcon.innerHTML = this.toIcon;this.endIcon.style.zIndex = '100';
+            this.endIcon.style.top = `${y2 + (toH / 2) - (parseInt(this.lineH) / 2) - 13.5}px`;
+            this.endIcon.style.left = `${x2 - this.toGap - 14}px`;
+            this.container.appendChild(this.endIcon);
+            this.els.push(this.endIcon);
+        }
+        // ************************************************
+
         if(this.style == 'linear'){
-            // let gap = (toE - fromE) / 4;
-            let gap = 0;
-            let rotate = 0;
-            // let rotate = (Math.atan(((y2 - toH / 2) - y1) / (x2 - x1 + gap)) * 100);
-            let radius = 0;
-            let deltaY = ((y2 + toH) - y1) / 2;
+            let deltaY = ((y2 + toH) - y1) / 2; // Posicionamento vertical da linha (centro dos dois elementos)
+            let rotate = Math.atan2((y2 + toH / 2) - (y1 + fromH / 2) + 3, (x2 - this.toGap) - (x1 + fromW + this.fromGap)) * 180 / Math.PI; // rotacao da linha
             let line = document.createElement('div'); line.style.height = this.lineH;line.style.position = 'absolute';line.style.backgroundColor = this.lineC;line.style.zIndex = '90';
             line.style.top = `${deltaY + y1}px`;
-            line.style.left = `${fromE + this.fromGap}px`;
-            line.style.width = `${deltaX}px`;
+            line.style.left = `${fromE + this.fromGap + 7}px`;
+            line.style.width = `${deltaX - 14}px`;
             line.style.transform = `skewY(${rotate}deg)`
             
             this.container.appendChild(line);
@@ -54,25 +68,11 @@ class jsELConnector{
             initial.style.top = `${y1 + (fromH / 2) - (parseInt(this.lineH) / 2)}px`;
             initial.style.left = `${fromE + this.fromGap}px`;
             initial.style.width = `${deltaX * this.breakpoint}px`;
-            if(this.fromIcon && this.fromIcon != ''){
-                this.startIcon = document.createElement('span');this.startIcon.style.position = 'absolute';this.startIcon.innerHTML = this.fromIcon;this.startIcon.style.zIndex = '100';
-                this.startIcon.style.top = `${y1 + (fromH / 2) - (parseInt(this.lineH) / 2) - 14}px`;
-                this.startIcon.style.left = `${fromE + this.fromGap}px`;
-                this.container.appendChild(this.startIcon);
-                this.els.push(this.startIcon);
-            }
-            
+
             let final = document.createElement('div'); final.style.height = this.lineH;final.style.position = 'absolute';final.style.backgroundColor = this.lineC;final.style.zIndex = '90';
             final.style.top = `${y2 + (toH / 2) - (parseInt(this.lineH) / 2)}px`;
             final.style.left = `${fromE + this.fromGap + (deltaX * this.breakpoint)}px`;
             final.style.width = `${deltaX * (1 - this.breakpoint) - (this.fromGap + this.toGap) - 1}px`;
-            if(this.toIcon &&  this.toIcon != ''){
-                this.endIcon = document.createElement('span');this.endIcon.style.position = 'absolute';this.endIcon.innerHTML = this.toIcon;this.endIcon.style.zIndex = '100';
-                this.endIcon.style.top = `${y2 + (toH / 2) - (parseInt(this.lineH) / 2) - 13.5}px`;
-                this.endIcon.style.left = `${x2 - this.toGap - 14}px`;
-                this.container.appendChild(this.endIcon);
-                this.els.push(this.endIcon);
-            }
             
             let central = document.createElement('div'); central.style.width = this.lineH;central.style.position = 'absolute';central.style.backgroundColor = this.lineC;central.style.zIndex = '90';
             let deltaY;
