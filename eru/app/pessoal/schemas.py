ece43d87085053864e2f5c4a8c36acf65a92377e
rafael <castelhano.rafael@gmail.com>
2026-02-16 12:00:09 -0400

    Ajustes Turno

--- eru/app/pessoal/schemas.py
+++ eru/app/pessoal/schemas.py
@@ -2,13 +2,28 @@ from pydantic import BaseModel, Field
 from django.utils.translation import gettext_lazy as _
 
 class FrequenciaSchema(BaseModel):
-    calcular_intervalos_jornada: bool = Field(
+    incluir_intervalos_jornada: bool = Field(
         False, 
         title=_("Incluir intervalos na jornada"),
         json_schema_extra={'format': 'checkbox'}
     )
 
+class FolhaSchema(BaseModel):
+    dia_fechamento: int = Field(default=30, ge=1, le=31)
+    permite_adiantamento: bool = Field(
+        False,
+        json_schema_extra={'format': 'checkbox'}
+        )
+    foo: bool = Field(
+        False,
+        json_schema_extra={'format': 'checkbox'}
+        )
+    bar: bool = Field(
+        True,
+        json_schema_extra={'format': 'checkbox'}
+        )
+
 class PessoalSettingsSchema(BaseModel):
-    model_config = {"title": " "}
+    model_config = {"title": _("Configurações Gerais")}
     frequencia: FrequenciaSchema = Field(default_factory=FrequenciaSchema, title=_("Frequência"))
-    # folha: FolhaSchema = Field(default_factory=FolhaSchema)
+    folha: FolhaSchema = Field(default_factory=FolhaSchema, title=_("Folha"))
